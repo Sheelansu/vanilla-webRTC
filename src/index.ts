586e6c4baee2147f487aee25e093dbd92d2d1637
Sheelansu <sheelansu.ujjain@hotmail.com>
2026-07-31 22:03:46 +0530

    wss configured as signaling server.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,64 @@
-import { WebSocketServer } from "ws"
+import { WebSocketServer, WebSocket } from "ws"
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws){
-    ws.on('error', console.error);
+type wsType = null |  WebSocket
+let senderWebSocket: wsType = null;
+let receiverWebSocket: wsType = null;
 
-    ws.on('message', function message(data: any){
+wss.on("connection", (ws) => {
+    ws.on("error", console.error);
+
+    ws.on("message", (data: any) => {
         const message = JSON.parse(data);
-    });
 
-    ws.send('Hello from WebSocketServer')
-}) 
+        switch (message.type) {
+            case "identify-as-sender":
+                senderWebSocket = ws;
+                break;
+
+            case "identify-as-receiver":
+                receiverWebSocket = ws;
+                break;
+
+            case "create-offer":
+                receiverWebSocket?.send(
+                    JSON.stringify({
+                        type: "offer",
+                        offer: message.offer,
+                    })
+                );
+                break;
+
+            case "create-answer":
+                senderWebSocket?.send(
+                    JSON.stringify({
+                        type: "answer",
+                        answer: message.answer,
+                    })
+                );
+                break;
+
+            case "ice-candidate":
+                if (ws === senderWebSocket) {
+                    receiverWebSocket?.send(
+                        JSON.stringify({
+                            type: "ice-candidate",
+                            candidate: message.candidate,
+                        })
+                    );
+                } else if (ws === receiverWebSocket) {
+                    senderWebSocket?.send(
+                        JSON.stringify({
+                            type: "ice-candidate",
+                            candidate: message.candidate,
+                        })
+                    );
+                }
+                break;
+
+            default:
+                console.warn(`Unknown message type: ${message.type}`);
+        }
+    });
+}); 
